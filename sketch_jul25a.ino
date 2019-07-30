910dd732dac358edb18eed2989aa83778890f5cb
jmaarbjerg <jacobmc@gmail.com>
2019-07-30 15:38:09 +0200

    Initial commit + base code

---
+++ sketch_jul25a.ino
@@ -0,0 +1,61 @@
+// color swirl! connect an RGB LED to the PWM pins as indicated
+// in the #defines
+// public domain, enjoy!
+ 
+#define REDPIN 5
+#define GREENPIN 6
+#define BLUEPIN 3
+#define REDPOT 1
+#define GREENPOT 2
+#define BLUEPOT 0
+
+#define FADESPEED 5     // make this higher to slow down
+ 
+void setup() {
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+  pinMode(REDPOT, INPUT);
+  pinMode(GREENPOT, INPUT);
+  pinMode(BLUEPOT, INPUT);
+}
+
+void loop(){
+  fade();
+  }
+ 
+ 
+void fade() {
+  int r, g, b;
+ 
+  // fade from blue to violet
+  for (r = 0; r < 256; r++) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from violet to red
+  for (b = 255; b > 0; b--) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from red to yellow
+  for (g = 0; g < 256; g++) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+  // fade from yellow to green
+  for (r = 255; r > 0; r--) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from green to teal
+  for (b = 0; b < 256; b++) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from teal to blue
+  for (g = 255; g > 0; g--) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+}
