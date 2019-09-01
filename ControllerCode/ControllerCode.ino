118724cb5c143981d0ec10df499b005a7c59b6ee
jmaarbjerg <jacobmc@gmail.com>
2019-09-01 12:28:47 +0200

    Added println for debugging

--- ControllerCode/ControllerCode.ino
+++ ControllerCode/ControllerCode.ino
@@ -47,12 +47,15 @@ void loop(){
   //Control for the MA part
   if(manuState == HIGH) {
     manual();
+    Serial.println("manu");
   }
   else if (autoState == HIGH && fadeState == HIGH){
     fade();
+    Serial.println("AutoFade");
   }
   else if (autoState == HIGH && discoState == HIGH){
     disco();
+    Serial.println("AutoDisco");
   }
 }
      
